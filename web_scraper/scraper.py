2c8b64508504477f2b3e30e6079fa272476adc7b
Grace Choi <choi.g.330@gmail.com>
2021-02-04 20:42:39 -0800

    fixed output

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -33,12 +33,14 @@ def get_citations_needed_report(url):
     body_results = soup.find(id='bodyContent')
     citation_results = body_results.find_all('p')
 
+    final_result = []
     for result in citation_results:
         section = result.find(title='Wikipedia:Citation needed')
         if section:
-            x = re.sub("\[?[0-9]?[0-9]?\]?(.*?)\[[0-9][0-9][0-9]?\]", "", result.text)
-            # print(f"Citation needed for:\n{x}")
-            return f"Citation needed for:\n{x}"
+            x = re.sub("\[?[0-9]?[0-9]?\]?(.*?)\[[0-9][0-9][0-9]?\]","",result.text)
+            x = x.strip('\n')
+            final_result.append(x)
+    return '\n'.join(final_result)
 
 def get_citations_needed_section(url):
     """String of passage that contains the missing citation. 
@@ -54,15 +56,15 @@ def get_citations_needed_section(url):
     soup = BeautifulSoup(content, 'html.parser')
     body_results = soup.find(id='bodyContent')
     citation_results = body_results.find_all('p')
-
+    
     for result in citation_results:
         section = result.find(title='Wikipedia:Citation needed')
         if section:
             x = re.sub("\[?[0-9]?[0-9]?\]?(.*?)\[[0-9][0-9][0-9]?\]", "", result.text)
-            # print(f"Relevant passage:\n{result.text}")
-            return f"Relevant passage:\n{result.text}"
+            print(f"Relevant passage:\n{result.text}")
+            # return f"Relevant passage:\n{result.text}"
 
-# if __name__ == '__main__':
-    # url = 'https://en.wikipedia.org/wiki/GameStop'
+if __name__ == '__main__':
+    url = 'https://en.wikipedia.org/wiki/GameStop'
     # print(get_citations_needed_count(url))
-    # get_citations_needed_report(url)
+    print(get_citations_needed_report(url))
